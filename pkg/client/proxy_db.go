24d3a745be4d576ea32cb7cfd5a6e1303782a413
Thibaud Suire <thibaud@loungeup.com>
2024-03-21 14:44:06 +0100

    [go] remove cache for reading booking from proxy-db

--- pkg/client/proxy_db.go
+++ pkg/client/proxy_db.go
@@ -8,16 +8,5 @@ import (
 type proxyDBClient struct{ baseClient *Client }
 
 func (c *proxyDBClient) ReadBooking(selector *models.BookingSelector) (*models.Booking, error) {
-	if cachedResult, ok := c.baseClient.eventuallyReadCache(selector.RID()).(*models.Booking); ok {
-		return cachedResult, nil
-	}
-
-	result, err := transport.GetRESModel[*models.Booking](c.baseClient.resClient, selector.RID())
-	if err != nil {
-		return nil, err
-	}
-
-	defer c.baseClient.eventuallyWriteCache(selector.RID(), result)
-
-	return result, nil
+	return transport.GetRESModel[*models.Booking](c.baseClient.resClient, selector.RID())
 }
