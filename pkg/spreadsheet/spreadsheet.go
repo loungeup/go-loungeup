bb48a80e2a352add463c1b14bd1aa6216176791a
Remy DUTHU <remy.duthu@gmail.com>
2024-08-28 15:56:40 +0200

    [go] Add spreadsheet package to work with XLSX files

---
+++ pkg/spreadsheet/spreadsheet.go
@@ -0,0 +1,79 @@
+// Package spreadsheet provides a unified way to interact with spreadsheets.
+package spreadsheet
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+
+	"github.com/xuri/excelize/v2"
+)
+
+type XLSXFile struct {
+	baseFile *excelize.File
+}
+
+func NewXLSXFile() *XLSXFile {
+	return &XLSXFile{
+		baseFile: excelize.NewFile(),
+	}
+}
+
+func (f *XLSXFile) Bytes() ([]byte, error) {
+	if err := f.deleteDefaultSheet(); err != nil {
+		return nil, err
+	}
+
+	var buffer bytes.Buffer
+	if err := f.baseFile.Write(&buffer); err != nil {
+		return nil, fmt.Errorf("could not write file to buffer: %w", err)
+	}
+
+	return buffer.Bytes(), nil
+}
+
+func (f *XLSXFile) AddSheet(name string) (*XLSXSheet, error) {
+	if _, err := f.baseFile.NewSheet(name); err != nil {
+		return nil, fmt.Errorf("could not add sheet to file: %w", err)
+	}
+
+	return &XLSXSheet{
+		baseFile:        f.baseFile,
+		currentRowIndex: 1,
+		name:            name,
+	}, nil
+}
+
+func (f *XLSXFile) deleteDefaultSheet() error {
+	if err := f.baseFile.DeleteSheet("Sheet1"); err != nil {
+		return fmt.Errorf("could not delete default sheet from file: %w", err)
+	}
+
+	return nil
+}
+
+type XLSXSheet struct {
+	baseFile        *excelize.File
+	currentRowIndex int
+	name            string
+}
+
+func (s *XLSXSheet) AddRows(rows ...[]string) error {
+	for _, row := range rows {
+		if err := s.addRow(row); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *XLSXSheet) addRow(row []string) error {
+	if err := s.baseFile.SetSheetRow(s.name, "A"+strconv.Itoa(s.currentRowIndex), &row); err != nil {
+		return fmt.Errorf("could not add row to sheet: %w", err)
+	}
+
+	s.currentRowIndex++
+
+	return nil
+}
---
+++ pkg/spreadsheet/spreadsheet_test.go
@@ -0,0 +1,39 @@
+package spreadsheet_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/loungeup/go-loungeup/pkg/spreadsheet"
+	"github.com/stretchr/testify/assert"
+	"github.com/xuri/excelize/v2"
+)
+
+func TestXLSXFile(t *testing.T) {
+	rows := [][]string{
+		{"First Name", "Last Name"},
+		{"John", "Doe"},
+		{"Jane", "Doe"},
+	}
+
+	file := spreadsheet.NewXLSXFile()
+
+	sheet, err := file.AddSheet("A Simple Sheet")
+	assert.NoError(t, err)
+	assert.NoError(t, sheet.AddRows(rows...))
+
+	data, err := file.Bytes()
+	assert.NoError(t, err)
+
+	t.Run("validate file content", func(t *testing.T) {
+		rawFile, err := excelize.OpenReader(bytes.NewReader(data))
+		assert.NoError(t, err)
+
+		assert.Equal(t, 1, rawFile.SheetCount)
+
+		rawRows, err := rawFile.GetRows("A Simple Sheet")
+		assert.NoError(t, err)
+		assert.Equal(t, rows, rawRows)
+	})
+
+}
