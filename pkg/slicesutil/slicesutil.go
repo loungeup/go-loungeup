17858455f500c49a05b4a875b55e1c3b4fc5ed94
Remy DUTHU <remy.duthu@gmail.com>
2024-09-05 18:24:15 +0200

    [go] Add slicesutil.ToAny function

--- pkg/slicesutil/slicesutil.go
+++ pkg/slicesutil/slicesutil.go
@@ -8,3 +8,12 @@ func Map[S ~[]E, E, M any](s S, f func(E) M) []M {
 
 	return result
 }
+
+func ToAny[S ~[]E, E any](s S) []any {
+	result := make([]any, len(s))
+	for i, v := range s {
+		result[i] = v
+	}
+
+	return result
+}
