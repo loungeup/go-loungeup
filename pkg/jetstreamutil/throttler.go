022408c7333694cef764b8f0878c80553ba41979
Remy DUTHU <remy.duthu@gmail.com>
2025-02-04 09:47:33 +0100

    [go] Add the ability to customize throttler interval

--- pkg/jetstreamutil/throttler.go
+++ pkg/jetstreamutil/throttler.go
@@ -2,6 +2,7 @@ package jetstreamutil
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"strings"
 	"sync"
@@ -9,14 +10,14 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/loungeup/go-loungeup/pkg/log"
+	"github.com/loungeup/go-loungeup/pkg/resutil"
 	"github.com/nats-io/nats.go/jetstream"
 )
 
 type Throttler struct {
-	interval         time.Duration
-	logger           *log.Logger
-	progressInterval time.Duration
-	throttledMsgs    sync.Map
+	interval      time.Duration
+	logger        *log.Logger
+	throttledMsgs sync.Map
 }
 
 func NewThrottler(options ...throttlerOption) *Throttler {
@@ -30,59 +31,62 @@ func NewThrottler(options ...throttlerOption) *Throttler {
 		option(result)
 	}
 
-	if result.progressInterval == 0 {
-		result.progressInterval = result.interval / 2 //nolint:gomnd,mnd
-	}
-
-	result.logger.Debug("Throttler created",
-		slog.String("interval", result.interval.String()),
-		slog.String("progressInterval", result.progressInterval.String()),
-	)
+	result.logger.Debug("Throttler created", slog.String("interval", result.interval.String()))
 
 	return result
 }
 
 func WithThrottlerInterval(interval time.Duration) throttlerOption {
-	return func(t *Throttler) { t.interval = interval }
+	return func(throttler *Throttler) { throttler.interval = interval }
 }
 
 func WithThrottlerLogger(logger *log.Logger) throttlerOption {
-	return func(t *Throttler) { t.logger = logger }
+	return func(throttler *Throttler) { throttler.logger = logger }
 }
 
-func WithThrottlerProgressInterval(progressInterval time.Duration) throttlerOption {
-	return func(t *Throttler) { t.progressInterval = progressInterval }
-}
-
-func (t *Throttler) Handle(next func(msg jetstream.Msg)) func(msg jetstream.Msg) {
+func (throttler *Throttler) Handle(next func(msg jetstream.Msg)) func(msg jetstream.Msg) {
 	return func(msg jetstream.Msg) {
 		key := strings.Join([]string{msg.Subject(), string(msg.Data())}, "-")
 
-		l1 := t.logger.With(
+		validThrottlerInterval := throttler.parseThrottlerInterval(msg.Data())
+
+		l1 := throttler.logger.With(
 			slog.Any("data", json.RawMessage(msg.Data())),
 			slog.String("key", key),
 			slog.String("subject", msg.Subject()),
 			slog.String("traceId", uuid.NewString()),
+			slog.String("validThrottlerInterval", validThrottlerInterval.String()),
 		)
 
-		if t.isLocked(key) {
+		if validThrottlerInterval == 0 {
+			l1.Debug("Processing message")
+			next(msg)
+			l1.Debug("Message processed")
+			throttler.release(key)
+
+			return
+		}
+
+		if throttler.isLocked(key) {
 			l1.Debug("Terminating duplicated message")
 			msg.Term()
 
 			return
 		}
 
-		t.lock(key)
+		throttler.lock(key)
 
-		timer := time.NewTimer(t.interval)
-		ticker := time.NewTicker(t.progressInterval)
+		l1.Debug("Throttling message")
+
+		timer := time.NewTimer(validThrottlerInterval)
+		ticker := time.NewTicker(validThrottlerInterval / 2) //nolint:all
 
 		go func() {
 			defer func() {
 				l1.Debug("Message processed")
 				timer.Stop()
 				ticker.Stop()
-				t.release(key)
+				throttler.release(key)
 			}()
 
 			for {
@@ -101,13 +105,50 @@ func (t *Throttler) Handle(next func(msg jetstream.Msg)) func(msg jetstream.Msg)
 	}
 }
 
-func (t *Throttler) isLocked(key string) bool {
-	_, result := t.throttledMsgs.Load(key)
+func (throttler *Throttler) parseThrottlerInterval(data []byte) time.Duration {
+	request := resutil.NewRequestWithParams(&throttlerParams{})
+	_ = json.Unmarshal(data, request)
+
+	if request.Params.throttlerInterval >= 0 && request.Params.throttlerInterval <= time.Minute {
+		return request.Params.throttlerInterval
+	}
+
+	return throttler.interval
+}
+
+func (throttler *Throttler) isLocked(key string) bool {
+	_, result := throttler.throttledMsgs.Load(key)
 	return result
 }
 
-func (t *Throttler) lock(key string) { t.throttledMsgs.Store(key, time.Now()) }
+func (throttler *Throttler) lock(key string) { throttler.throttledMsgs.Store(key, time.Now()) }
 
-func (t *Throttler) release(key string) { t.throttledMsgs.Delete(key) }
+func (throttler *Throttler) release(key string) { throttler.throttledMsgs.Delete(key) }
 
-type throttlerOption func(*Throttler)
+type throttlerOption func(throttler *Throttler)
+
+type throttlerParams struct {
+	throttlerInterval time.Duration
+}
+
+var _ (json.Unmarshaler) = (*throttlerParams)(nil)
+
+func (params *throttlerParams) UnmarshalJSON(data []byte) error {
+	type jsonModel struct {
+		ThrottlerInterval string `json:"throttlerInterval"`
+	}
+
+	model := &jsonModel{}
+	if err := json.Unmarshal(data, model); err != nil {
+		return fmt.Errorf("could not decode throttler handler params: %w", err)
+	}
+
+	throttlerInterval, err := time.ParseDuration(model.ThrottlerInterval)
+	if err != nil {
+		return fmt.Errorf("could not parse throttler interval: %w", err)
+	}
+
+	*params = throttlerParams{throttlerInterval: throttlerInterval}
+
+	return nil
+}
