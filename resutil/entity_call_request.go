e877e3fd2923a71587a42bd96781744282745182
Remy DUTHU <remy.duthu@gmail.com>
2025-04-22 11:27:00 +0200

    [go] Add 'EntityCallRequest' type

---
+++ resutil/entity_call_request.go
@@ -0,0 +1,66 @@
+package resutil
+
+import (
+	"github.com/jirenius/go-res"
+	lumodels "github.com/loungeup/go-loungeup/client/models"
+	"github.com/loungeup/go-loungeup/errors"
+	"github.com/loungeup/go-loungeup/log"
+)
+
+// EntityCallRequest wraps a res.CallRequest with an associated Entity. This structure allows handlers to access the
+// entity directly, reducing the need for repeated entity lookups and improving code clarity.
+type EntityCallRequest struct {
+	res.CallRequest
+
+	Entity *lumodels.Entity
+}
+
+type EntityCallHandler func(request *EntityCallRequest)
+
+type EntityCallRequestConfig struct {
+	// PathParamKey used to extract the entity ID from the request path parameters.
+	PathParamKey string
+}
+
+type EntityCallRequestOption func(config *EntityCallRequestConfig)
+
+type EntityReader interface {
+	ReadEntity(selector *lumodels.EntitySelector) (*lumodels.Entity, error)
+}
+
+func WithEntityCallRequestPathParamKey(key string) EntityCallRequestOption {
+	return func(config *EntityCallRequestConfig) { config.PathParamKey = key }
+}
+
+// WithEntityCallHandler wraps a handler to include entity resolution logic. This function ensures that the entity is
+// resolved and passed to the handler, simplifying the handler's responsibilities and centralizing error handling.
+func WithEntityCallHandler(next EntityCallHandler, reader EntityReader, options ...EntityCallRequestOption) res.CallHandler {
+	config := &EntityCallRequestConfig{
+		PathParamKey: "entityID",
+	}
+
+	for _, option := range options {
+		option(config)
+	}
+
+	return func(request res.CallRequest) {
+		entityID, err := ParseUUIDPathParam(request, config.PathParamKey)
+		if err != nil {
+			errors.LogAndWriteRESError(log.Default(), request, err)
+
+			return
+		}
+
+		entity, err := reader.ReadEntity(&lumodels.EntitySelector{EntityID: entityID})
+		if err != nil {
+			errors.LogAndWriteRESError(log.Default(), request, err)
+
+			return
+		}
+
+		next(&EntityCallRequest{
+			CallRequest: request,
+			Entity:      entity,
+		})
+	}
+}
